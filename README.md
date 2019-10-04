fcce2d6e70a76dc4f843857ec3ee6c26b8c17c83
Merwane Draï <merwanedr@gmail.com>
2019-10-04 23:47:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Gilfoyle's notifier when Bitcoin drops bellow a certain price.
+
+> Oh that's the song **You suffer by Napalm Death.**
+
+### Usage:
+
+```
+pip install requirements.txt
+
+python alert.py
+```
+
+By keeping the worker on background, it should notify you whenever Bitcoin drops bellow a specific price.
+
+It also displays a banner for Mac users.
+
+### Donate
+
+**Bitcoin:** 1HHMRuezg1WAPqGRQ3S8xFKGgmMHZZrCoa
+
+**Bitcoin Cash:** qrxjktfjdse3ll0ttrll20gykuhqjw764queg3w2tj
+
+
+
+<img src="gilf.gif">
---
+++ alert.py
@@ -0,0 +1,54 @@
+import requests
+import schedule
+import time
+from pydub import AudioSegment
+from pydub.playback import play
+import os
+import sys
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+    print("")
+
+cls()
+
+value = input("Enter the price you bought your Bitcoins in USD: ")
+
+cls()
+
+print("Worker launched, you can leave it in background.")
+
+BELLOW = float(value)
+
+song = AudioSegment.from_mp3('suffer.mp3')
+
+inc = 1
+
+def req_price():
+    r = requests.get('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
+    return r.json()['bpi']['USD']['rate_float']
+
+def alert(price):
+    if price < float(BELLOW):
+        # mac notification
+        if sys.platform == 'darwin':
+            os.system("""
+                    osascript -e 'display notification "{}" with title "{}"'
+                    """.format("Current price: $"+str(price), "Bitcoin price dropped bellow $"+str(BELLOW)))
+        play(song)
+
+def main():
+    global inc
+
+    if inc == 1:
+        alert(req_price())
+        inc += 1
+
+    if float(BELLOW) < req_price():
+        inc -= 1
+
+schedule.every(5).seconds.do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
